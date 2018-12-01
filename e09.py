add912a3b6e73b3a63b066ed9f416e456c489257
Lenka Kapinová <l.kapinova@gmail.com>
2018-12-01 09:31:47 +0100

    Guessing and comparing numbers - one round only

--- e09.py
+++ e09.py
@@ -1,1 +1,13 @@
-# Init
+import random
+
+pc = random.randint(1,9)
+player = int(input("Guess the number from 1 to 9: "))
+
+if player == pc:
+    print ("Your guess was exactly right!")
+if player < pc:
+    print("Your guess was too low!")
+if player > pc:
+    print("Your guess was too high`!")
+
+# print (pc, player)
