1f72d4ec153ce4a557b1b2138c1615b6d7194b3f
Lenka Kapinová <l.kapinova@gmail.com>
2018-12-01 10:41:56 +0100

    Loop added

--- e09.py
+++ e09.py
@@ -1,13 +1,31 @@
 import random
 
-pc = random.randint(1,9)
-player = int(input("Guess the number from 1 to 9: "))
+a = 0
 
-if player == pc:
-    print ("Your guess was exactly right!")
-if player < pc:
-    print("Your guess was too low!")
-if player > pc:
-    print("Your guess was too high`!")
+while True:
+    pc = random.randint(1,9)
+    player = input("Guess the number from 1 to 9 (or type exit): ")
+
+    if player.isdigit():
+        player = int(player)
+        if player == pc:
+            print ("Your guess was exactly right!")
+        if player < pc:
+            print("Your guess was too low!")
+        if player > pc:
+            print("Your guess was too high!")
+
+    else:
+        player = str(player)
+        exit="exit"
+        if player != exit:
+            print ("Invalid input.")
+        
+
+    if player == exit:
+        print ("Goodby!")
+        break
+    a += 1
+print ("You've played %d times." % a)
 
 # print (pc, player)
