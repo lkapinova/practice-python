80748e0d289ec74cdc72def2d126c975cf23d204
Lenka Kapinová <l.kapinova@gmail.com>
2018-12-03 17:21:20 +0100

    First solution

--- e10.py
+++ e10.py
@@ -1,3 +1,5 @@
+# https://www.practicepython.org/exercise/2014/04/10/10-list-overlap-comprehensions.html
+
 #a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 #b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
