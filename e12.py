7b040b982d28d53144961d9d18c484bd44073d27
Lenka Kapinová <l.kapinova@gmail.com>
2018-12-06 14:07:49 +0100

    Solution with a function

---
+++ e12.py
@@ -0,0 +1,16 @@
+# https://www.practicepython.org/exercise/2014/04/25/12-list-ends.html
+
+
+def first_last_el (a):
+    b = [a[0], a[-1]]
+    return b
+
+
+a = [5, 10, 15, 20, 25]
+
+b = first_last_el(a)
+
+print (b)
+
+
+
