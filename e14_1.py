bf01f649f6e1a69128be252a52c21d032bdf9a89
Lenka Kapinová <l.kapinova@gmail.com>
2018-12-11 21:57:26 +0100

    Using set

--- e14_1.py
+++ e14_1.py
@@ -3,15 +3,10 @@
 a = [1, 1, 5, 4, 6, 8, 4, 2, 7, 9, 5, 3, 2, 5, 6, 7, 8]
 
 
-# a = set(a)
-# print (a)
-
 def dupl(a):
-    b = []
-    for element in a:
-        if element not in b:
-            b.append(element)
-    return b
+    a = set(a)
+    a = list(a)
+    return(a)
 
 
 print(dupl(a))
