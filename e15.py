12e7456583b3c9e168169d88a157386b97d11496
Lenka Kapinová <l.kapinova@gmail.com>
2018-12-11 23:18:57 +0100

    def solution without print settings

---
+++ e15.py
@@ -0,0 +1,13 @@
+# https://www.practicepython.org/exercise/2014/05/21/15-reverse-word-order.html
+
+string = str(input("Write here a long string: "))
+print(string)
+
+def Reverse_str(string):
+    string = string.split(" ")
+    rev_str = string[-1::-1]
+    return rev_str
+
+
+print(Reverse_str(string))
+
