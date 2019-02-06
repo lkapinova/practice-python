168cd4b036bb400e24c5dd84129ee594028bedc2
Lenka Kapinová <l.kapinova@gmail.com>
2019-02-06 22:40:47 +0100

    Guess letters - hangman game

--- e31.py
+++ e31.py
@@ -1,47 +1,50 @@
 # https://www.practicepython.org/exercise/2017/01/02/31-guess-letters.html
 
 
-def lenght_of_word(word):
-    word_lenght = 0
-    for i in word:
-        word_lenght += 1
+def word_in_dots(word):
+    # word_lenght = 0
+    # for i in word:
+    #     word_lenght += 1
+    word_lenght = len(word)
     a = "-"
-    a = word_lenght*a
-    print(a)
-    return a
+    print(word_lenght*a)
+    return word_lenght*a
 
-
-def charposition(word, letter_guess):
+def letter_position(word, letter_guess):
     pos = []
     for n in range(len(word)):
         if word[n] == letter_guess:
             pos.append(n)
-    print(pos)
     return pos
 
-if __name__ == "__main__":
-
-    # word = "Lenke"
-    # lenght_of_word(word)
-    # letter_guess = input("Try to find a word. Enter a letter that could be a part of it: ")
-    # charposition(word, letter_guess)
-
-    # a = "-----"
-    # pos = [1,4]
-    # letter_guess = "e"
-    # a = a.replace("-", letter_guess,pos[1])
-    # print(a)
-
-    a = "-----"
-    pos = [1,4]
-    # for i in pos:
-    #     a = a[:(pos[i])] + 'e' + a[(pos[i]+1):]
-    #     print(a)
-
+def letter_placing(a,pos):
     a_as_list = list(a)
     for i in range(0, len(a)):
         if i in pos:
-            a_as_list[i] = 'e'
+            a_as_list[i] = letter_guess
+    print("".join(a_as_list))
+    return "".join(a_as_list)
 
-    b = "".join(a_as_list)
-    print(a)
+#-----------------------------------------------------------------
+
+if __name__ == "__main__":
+
+    word = "cenichac"
+    a = word_in_dots(word)
+    used_letters = []
+
+    while True:
+        if a.find('-') != -1:
+            letter_guess = input("Try to find a word. Enter a letter that could be a part of it: ")
+            if letter_guess in used_letters:
+                print("You have already guessed the letter.")
+            if letter_guess not in used_letters:
+                used_letters.append(letter_guess)
+
+        pos = letter_position(word, letter_guess)
+        a = letter_placing(a,pos)
+    
+        if a.find('-') == -1:
+            break
+    
+    print("Great! The word is %s." %a)
