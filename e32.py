e1f5a0213f1a5269dca22dc70f525734f112f95e
Lenka Kapinová <l.kapinova@gmail.com>
2019-02-16 13:20:59 +0100

    Hangman to be added

--- e32.py
+++ e32.py
@@ -13,17 +13,6 @@ def word_in_dots(word):
     print(word_lenght*a)
     return word_lenght*a
 
-def letter_position(word, letter_guess):
-    pos = []
-    incorrect_guesses = 0
-
-    for n in range(len(word)):
-        if word[n] == letter_guess:
-            pos.append(n)
-        else:
-            incorrect_guesses += 1
-    return pos, incorrect_guesses
-
 def letter_placing(a,pos,letter_guess):
     a_as_list = list(a)
     for i in range(0, len(a)):
@@ -47,19 +36,28 @@ def play_game():
     word = random_word()
     a = word_in_dots(word)
     used_letters = []
-    
+    incorrect_guesses = 0
+
     while True:
         if a.find('-') != -1:
             letter_guess = input("Try to find a word. Enter a letter that could be a part of it: ")
+            pos = []
             if letter_guess in used_letters:
                 print("You have already guessed the letter.")
             if letter_guess not in used_letters:
                 used_letters.append(letter_guess)   
-        
-        pos, incorrect_guesses = letter_position(word, letter_guess)
-        a = letter_placing(a,pos,letter_guess)
 
-        if incorrect_guesses == 6:
+        for n in range(len(word)):
+            if word[n] == letter_guess:
+                pos.append(n)
+
+        if pos == []:
+            incorrect_guesses += 1
+
+        a = letter_placing(a,pos,letter_guess)
+        
+
+        if incorrect_guesses >= 6:
             print("You have lost the game.")
             return
     
